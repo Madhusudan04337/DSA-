e429472e12a1ec2413aec88db8480c7d3abd47bc
Madhusudan <madhusudan04337@gmail.com>
2025-07-16 20:51:09 +0530

    probleam fixed

--- 3_bitwise_&_loops/char2.cpp
+++ 3_bitwise_&_loops/char2.cpp
@@ -9,6 +9,7 @@ int main(){
     int total_digits=0;
     int total_alpha = 0;
     int other_char = 0;
+    int total_spaces = 0;
 
     while(ch!='$'){
         if (isdigit(ch)){
@@ -16,6 +17,9 @@ int main(){
         }
         else if(isalpha(ch)){
             total_alpha++;
+        }
+        else if (ch == ' ' || ch == '\t'){
+            total_spaces++;
         }
         else{
             other_char++;
@@ -27,5 +31,6 @@ int main(){
     cout<<"Total digits = "<<total_digits<<endl;
     cout<<"Total alphabets = "<<total_alpha<<endl;
     cout<<"Other characters = "<<other_char<<endl;
+    cout<<"Total spaces = "<<total_spaces;
     return 0;
 }
